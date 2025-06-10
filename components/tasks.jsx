9e93cb0f84c441c10089bed45e47be508dc26651
xDAGAx13 <rohandaga05@gmail.com>
2025-06-10 15:12:37 +0530

    updated priority and fixed flex containers for tasks

--- components/tasks.jsx
+++ components/tasks.jsx
@@ -1,68 +1,81 @@
+import FontAwesome from "@expo/vector-icons/FontAwesome";
 import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import * as Notifications from "expo-notifications";
 import { useEffect, useState } from "react";
-import { ScrollView, Text, TextInput, TouchableOpacity, View } from "react-native";
-import * as Notifications from 'expo-notifications'
+import {
+  ScrollView,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import Priority from "./priority";
 
 export default function Tasks() {
   const [tasks, setTasks] = useState([]);
   const [addTask, setAddTask] = useState("");
   const [showTasks, setShowTasks] = useState(false);
-  const [priority, setPriority] = useState('');
-  
+  const [priority, setPriority] = useState("");
 
-  useEffect(()=>{
-    const loadTasks = async()=>{
-      try{
-        const savedTasks = await AsyncStorage.getItem('tasks');
-        if(savedTasks){
+  useEffect(() => {
+    const loadTasks = async () => {
+      try {
+        const savedTasks = await AsyncStorage.getItem("tasks");
+        if (savedTasks) {
           setTasks(JSON.parse(savedTasks));
         }
-      }catch(e){
-        console.error('Failed to load tasks: ',e)
+      } catch (e) {
+        console.error("Failed to load tasks: ", e);
       }
-    }
+    };
     loadTasks();
-  }, [])
+  }, []);
 
-  useEffect(()=>{
-    const saveTasks = async()=>{
-      try{
-        await AsyncStorage.setItem('tasks', JSON.stringify(tasks));
-      }catch(e){
-        console.error('Failed to save tasks: ',e);
+  useEffect(() => {
+    const saveTasks = async () => {
+      try {
+        await AsyncStorage.setItem("tasks", JSON.stringify(tasks));
+      } catch (e) {
+        console.error("Failed to save tasks: ", e);
       }
-    }
+    };
     saveTasks();
-  },[tasks])
+  }, [tasks]);
 
-  const scheduleNotification = async (taskText)=>{
-
-    const triggerTime = new Date(Date.now()+10*1000)
+  const scheduleNotification = async (taskText) => {
+    const triggerTime = new Date(Date.now() + 10 * 1000);
     const id = await Notifications.scheduleNotificationAsync({
-      content:{
-        title:'Task Reminder',
+      content: {
+        title: "Task Reminder",
         body: `Time to complete: ${taskText}`,
       },
-      trigger:triggerTime
-    })
+      trigger: triggerTime,
+    });
 
     return id;
-  }
+  };
 
   const handleAddTask = async () => {
     if (addTask.trim() === "") {
       alert("Cannot add empty task");
       return;
+    } else if (priority === "") {
+      alert("Please select the priority");
+      return;
     }
 
     const notificationId = await scheduleNotification(addTask.trim());
-    const newTask = {text: addTask.trim(), checked:false, priority:'',notificationId};
+    const newTask = {
+      text: addTask.trim(),
+      checked: false,
+      priority: priority,
+      notificationId,
+    };
     setTasks([...tasks, newTask]);
     setAddTask("");
-
+    setPriority("");
   };
 
   const handleUpdateCheckedTasks = async (index) => {
@@ -73,25 +86,30 @@ export default function Tasks() {
 
     const currentTask = updatedTasks[index];
 
-    if(currentTask.checked&&currentTask.notificationId){
-      try{
-        await Notifications.cancelScheduledNotificationAsync(currentTask.notificationId);
-        currentTask.notificationId=null;
-      }catch(e){
-        console.error('Failed to cancel notification: ',e)
+    if (currentTask.checked && currentTask.notificationId) {
+      try {
+        await Notifications.cancelScheduledNotificationAsync(
+          currentTask.notificationId
+        );
+        currentTask.notificationId = null;
+      } catch (e) {
+        console.error("Failed to cancel notification: ", e);
       }
     }
     setTasks(updatedTasks);
   };
 
+  const handleEditTask=(index)=>{
+    
+  }
 
-  const handleDeleteTask = (index)=>{
+  const handleDeleteTask = (index) => {
     const newTasks = [...tasks];
     newTasks.splice(index, 1);
     setTasks(newTasks);
-  }
+  };
   return (
-    <ScrollView className="min-h-screen" >
+    <ScrollView className="min-h-screen">
       {/* Current Tasks Title */}
       <View className=" pt-16 flex flex-col">
         <Text className="text-white ml-10 text-4xl font-semibold">
@@ -99,20 +117,21 @@ export default function Tasks() {
         </Text>
         <View className="flex-col gap-2 mt-2 mx-4 h-auto">
           <View className="flex-row items-center gap-2">
-          <TextInput
-            value={addTask}
-            onChangeText={setAddTask}
-            className="bg-white px-4 py-2 text-xl rounded-2xl flex-1 placeholder:text-gray-400 h-16 items-center justify-center border-4 border-black"
-            placeholder="Add a Task"
-            
-          />
-          <TouchableOpacity onPress={handleAddTask} className="text-white">
-            <FontAwesome6 name="plus" size={30} color="white" />
-          </TouchableOpacity>
+            <TextInput
+              value={addTask}
+              onChangeText={setAddTask}
+              className="bg-white px-4 py-2 text-xl rounded-2xl flex-1 placeholder:text-gray-400 h-16 items-center justify-center border-4 border-black"
+              placeholder="Add a Task"
+            />
+            <TouchableOpacity onPress={handleAddTask} className="text-white">
+              <FontAwesome6 name="plus" size={30} color="white" />
+            </TouchableOpacity>
           </View>
-          {addTask&&<Priority/>}
+          {addTask && (
+            <Priority priority={priority} setPriority={setPriority} />
+          )}
         </View>
-        
+
         <TouchableOpacity
           onPress={() =>
             showTasks === false ? setShowTasks(true) : setShowTasks(false)
@@ -128,16 +147,48 @@ export default function Tasks() {
           tasks.map((task, index) => (
             <View
               key={index}
-              className="flex-row mb-2 p-3 bg-white rounded-xl justify-between h-16 items-center border-4 border-gray-800"
+              className="flex-row mb-2 p-3 bg-white rounded-xl justify-between h-auto items-center border-4 border-gray-800"
             >
-              <Text className={`text-black text-xl font-semibold ${task.checked&&'line-through decoration-4'}`}>{task.text}</Text>
+              <View className="flex-1 pr-2">
+                <Text
+                  className={`text-black text-xl font-semibold flex-wrap ${
+                    task.checked && "line-through decoration-4"
+                  }`}
+                >
+                  {task.text}
+                  </Text>
+                  <Text className="">Priority:{" "}
+                  <FontAwesome
+                    name="circle"
+                    size={16}
+                    color={
+                      task.priority === "High"
+                        ? "red"
+                        : task.priority === "Medium"
+                        ? "orange"
+                        : "green"
+                    }
+                  />
+                </Text>
+              </View>
+
               <View className="flex-row gap-2 ">
-              <TouchableOpacity onPress={()=>handleUpdateCheckedTasks(index)}>
-                <Ionicons name={task.checked?"checkbox":'checkbox-outline'} color={task.checked?"green":'black'} size={30} />
-              </TouchableOpacity>
-              <TouchableOpacity onPress={()=>handleDeleteTask(index)}>
-                <Ionicons name='trash-outline' color="black" size={30} />
-              </TouchableOpacity>
+                <TouchableOpacity
+                  onPress={() => handleUpdateCheckedTasks(index)}
+                >
+                  <Ionicons
+                    name={task.checked ? "checkbox" : "checkbox-outline"}
+                    color={task.checked ? "green" : "black"}
+                    size={30}
+                  />
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => handleEditTask(index)}>
+                  <Ionicons name="pencil" color="black" size={26} />
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => handleDeleteTask(index)}>
+                  <Ionicons name="trash-outline" color="black" size={30} />
+                </TouchableOpacity>
+                
               </View>
             </View>
           ))}
